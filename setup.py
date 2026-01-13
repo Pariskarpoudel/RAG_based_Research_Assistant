62f2e7d62b1119cfe4f97ac7e4e6720d063e0bdb
Pariskar <pariskar2001@gmail.com>
2026-01-13 22:07:47 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="RAG_based_Research_Assistant",
+    version="0.1.0",
+    author="Pariskar Poudel",
+    author_email="079bct055.pariskar@pcampus.edu.np",
+    packages=find_packages(),
+    install_requires=[]
+)
