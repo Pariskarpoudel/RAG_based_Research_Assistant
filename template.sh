c0e5fa92f3e8c7d329d04a81423b303115401dbb
Pariskar <pariskar2001@gmail.com>
2026-01-13 19:29:26 +0545

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# creating directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py   # constructor file
+touch src/helper.py 
+touch src/prompt.py 
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully."  
+
+# now open gitbash in this directory and type : 
+# sh template.sh
